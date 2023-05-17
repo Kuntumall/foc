68676de3c916347be04671996968410acc2c384d
Kuntumall <112718299+Kuntumall@users.noreply.github.com>
2023-05-17 08:37:55 +0530

    Add files via upload

---
+++ foc 25.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int num1,num2;
+	printf("enter the num1 :");
+	scanf("%d",&num1);
+		
+			scanf("%d",&num2);
+			if (num1>num2)
+			printf("the num1 is greater :");
+			else
+			printf("the num2 is greater ");
+		return 0;
+}
